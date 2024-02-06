ff231fc363a8d74cef19b6944be635dd3e3e6125
Jesse Henson <29315680+JesseHenson@users.noreply.github.com>
2024-02-06 03:59:20 +0000

    Update dependencies and refactor code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,23 @@
-import streamlit as st
-from PyPDF2 import PdfFileReader, PdfFileWriter,PdfReader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_community.vectorstores import FAISS
-from langchain_openai import OpenAIEmbeddings
-from langchain.chains.question_answering import load_qa_chain
-from langchain_core.prompts import ChatPromptTemplate
-from langchain.llms import OpenAI
-from langchain_openai import ChatOpenAI
+from langchain import hub
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain.chains import RetrievalQA
 from langchain.docstore.document import Document
-from langchain.callbacks import get_openai_callback
-from langchain_core.runnables import RunnableLambda
-from langchain_core.output_parsers import StrOutputParser
+from langchain_community.vectorstores import FAISS
+import streamlit as st
+from pypdf import PdfReader
 
 
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+1. Please enter your OpenAI API Key to use the app
 """
 
-llm = OpenAI(temperature=0)
-embeddings = OpenAIEmbeddings()
+questions = {"Likelihood" : """
+    How well do my skills fit this job on a scale from 1-100? Again only output a single number score.
+        """     
+    }
 
-job_description = '''
+job_description_example = '''
 The project is to analyze the nonces submitted by different types of hardware and create an identifying signature for each of the hardware.
 An expert ML person is required.
 Person should be an expert programmer and be able to pull data, do the anlaysis, build the models and create a usable dashboard or excel for the end-user.
@@ -36,88 +26,50 @@ Good knowledge of c++ or firmware is desirable.
 Person should be available full-time and be flexible for calls over weekends and late evenings
 '''
 
-def get_resume(pdf):
-    if pdf is not None:
+
+resume_db = None
+job_description = None
+resume_retriever = None
+
+open_ai_api_key = st.text_input("Enter your OpenAI API Key to use")
+
+
+if open_ai_api_key != "":
+    embeddings = OpenAIEmbeddings(api_key=open_ai_api_key)
+    prompt = hub.pull("rlm/rag-prompt")
+    llm = ChatOpenAI(model_name="gpt-4", api_key=open_ai_api_key, temperature=0)
+
+
+if open_ai_api_key != "" and job_description != "":
+    st.write("2. You can now upload your resume")
+    pdf = st.file_uploader("Upload your PDF", type='pdf')
+    if pdf is not None and open_ai_api_key != "":
         pdf_reader = PdfReader(pdf)
 
         text = ""
         for page in pdf_reader.pages:
             text+= page.extract_text()
         
-        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
         docs = [Document(page_content=x) for x in text_splitter.split_text(text)]
+        resume_db = FAISS.from_documents(docs, embeddings)
+        st.write("Resume uploaded")
+        resume_retriever = resume_db.as_retriever()
 
-        with get_openai_callback() as cb:
-            resume_db = FAISS.from_documents(docs, embeddings)
-            print(f'Resume embeding: {cb}')
-        return resume_db
-
-            
-def get_job_description(job_description):
-    if job_description:
-        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-        docs = [Document(page_content=x) for x in text_splitter.split_text(job_description)]
-        with get_openai_callback() as cb:
-            job_description_db = FAISS.from_documents(docs, embeddings)
-            print(f'job embeding: {cb}')
-        return job_description_db
-
-
-def get_question():
-    questions = {"Likelihood" : """
-    How well do my skills fit this job on a scale from 1-100? Again only output a single number score.
-        """
-
-    
-        
-    }
-    return questions
-
-def retrive_docs(resume_db, job_description_db, query):
-    print(resume_db, job_description_db, query)
-    if job_description_db and resume_db:
-        job_description_docs = job_description_db.similarity_search(query=query,k=3)
-        resume_docs = resume_db.similarity_search(query=query,k=3)
-        # retrieved_docs.append(resume_docs)
-        return (job_description_docs, resume_docs)
-
-    
-
-def main():
-    prompt = ChatPromptTemplate.from_template("""
-        Based on my resume data ending with a [[]]:
-        Resume: {resume}
-        [[]]
-        and on the job description ending with a &^%:
-        Job Decsription: {job_description}
-        &^%
-        and answer the following question:
-        Question: {question}
-        """)
+if open_ai_api_key != "" and resume_db is not None:
+    st.write('3. Now you can copy and paste the job description')
     job_description = st.text_input("Copy and paste the upwork job description")
-    pdf = st.file_uploader("Upload your PDF", type='pdf')
-    model = ChatOpenAI(model="gpt-4")
-    output_parser = StrOutputParser()
-    chain = prompt | model | output_parser
 
-    resume_db = get_resume(pdf)
-    job_description_db = get_job_description(job_description)
-    question = get_question()
-    for question_name, question_text in question.items():
-        try:
-            job_description_docs, resume_docs = retrive_docs(resume_db, job_description_db, question_text)
-        except Exception as e:
-            print("this exception was caught")
-            
-        if job_description_docs and resume_docs and question:
-            with get_openai_callback() as cb:
-                response = chain.invoke({"resume": resume_docs, 
-                                        "job_description": job_description_docs, 
-                                        "question":question_text
-                })
-                print(f'response: {cb}')
-            st.write(f'{question_name}: {response}')
-        
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
 
-if __name__ == "__main__":
-    main()
+
+if resume_retriever is not None and job_description != "" and job_description is not None and open_ai_api_key is not None:
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="refine", retriever=resume_retriever, return_source_documents=False)
+    st.write("Calculating the score")
+    result = qa.run({'query': questions["Likelihood"], 
+                     'context':job_description
+                     })
+    st.write(result)
+
+
